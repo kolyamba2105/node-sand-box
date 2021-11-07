7358b930bddf9f2dc7f7b2e12b61ad2a32a947e0
kolyamba2105 <haravetskyy@gmail.com>
2021-11-07 13:18:56 +0200

    Add fp-ts dependencies

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:fp-ts/all',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    project: ['./tsconfig.json'],
+  },
+  plugins: ['@typescript-eslint', 'fp-ts'],
+  root: true,
+}

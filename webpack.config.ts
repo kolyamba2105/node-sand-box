03a2d55b77716b4460f58952611f97c25fa8ddd8
kolyamba2105 <haravetskyy@gmail.com>
2024-03-03 21:54:33 +0200

    Adjust webpack config

--- webpack.config.ts
+++ webpack.config.ts
@@ -3,8 +3,7 @@ import EslintWebpackPlugin from "eslint-webpack-plugin";
 import ForkTsCheckerWebpackPlugin from "fork-ts-checker-webpack-plugin";
 import NodemonWebpackPlugin from "nodemon-webpack-plugin";
 import * as path from "path";
-import * as webpack from "webpack";
-import "webpack-dev-server";
+import { Configuration, WebpackPluginInstance } from "webpack";
 import WebpackNodeExternals from "webpack-node-externals";
 
 const modes = ["development", "production"] as const;
@@ -18,43 +17,37 @@ function isValidMode(value: string): value is Mode {
 const env = process.env.NODE_ENV;
 const mode = env && isValidMode(env) ? env : "development";
 
-function isNonNullable<T>(value: T): value is NonNullable<T> {
-  return value !== null && value !== undefined;
+const plugins: Array<WebpackPluginInstance> = [
+  new CleanWebpackPlugin(),
+  new EslintWebpackPlugin({ extensions: ["ts"], files: "src" }),
+  new ForkTsCheckerWebpackPlugin(),
+];
+
+if (mode === "development") {
+  const nodemonWebpackPlugin = new NodemonWebpackPlugin({
+    ext: "js",
+    quiet: true,
+    script: "build/index.js",
+    watch: ["build"],
+  });
+
+  plugins.push(nodemonWebpackPlugin);
 }
 
-function compact<T>(values: Array<T>): Array<NonNullable<T>> {
-  return values.filter(isNonNullable);
-}
-
-const config: webpack.Configuration = {
+const config: Configuration = {
   devtool: mode === "development" ? "source-map" : false,
   entry: [path.resolve(__dirname, "src/index.ts")],
   externals: [WebpackNodeExternals()],
   mode,
   module: {
-    rules: [
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/,
-        type: "asset/resource",
-      },
-      {
-        exclude: /node_modules/,
-        test: /\.(ts|tsx)$/,
-        use: "esbuild-loader",
-      },
-    ],
+    rules: [{ exclude: /node_modules/, test: /\.(ts)$/, use: "esbuild-loader" }],
   },
   output: {
     clean: true,
     filename: "index.js",
     path: path.resolve(__dirname, "build"),
   },
-  plugins: compact([
-    new CleanWebpackPlugin(),
-    new EslintWebpackPlugin({ extensions: ["ts"], files: "src" }),
-    new ForkTsCheckerWebpackPlugin(),
-    mode === "development" ? new NodemonWebpackPlugin({ quiet: true }) : null,
-  ]),
+  plugins,
   resolve: {
     alias: {
       "~": path.resolve(__dirname, "src"),
